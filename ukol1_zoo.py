39b55256d73a55bd000a65cac4bdd924cac7374d
Iveta-eli <145855302+Iveta-eli@users.noreply.github.com>
2024-04-26 19:11:13 +0200

    5.verze
    Mělo by být ok.

--- ukol1_zoo.py
+++ ukol1_zoo.py
@@ -10,7 +10,6 @@ class Zvire:
         self.vaha = vaha
 
     def __str__(self):
-        #return f"Název zvířete: {self.druh}, jméno: {self.jmeno}, váha: {self.vaha}."
         return f"Jméno zvířete: {self.jmeno}, druh: {self.druh}, váha. {self.vaha} kg"
 
     def export_to_dict(self):
@@ -34,20 +33,21 @@ for zvire in seznam_zvirat:
 pavouk = Zvire(jmeno='Adolf', druh='Tarantule Velká', vaha=0.1)
 pavouk_export = pavouk.export_to_dict()
 
+zvirata = [Zvire(**zvire_data) for zvire_data in zvirata_dict]
+
+"""
 print(pavouk)
 print(pavouk.export_to_dict())
 
 assert pavouk_export['jmeno'] == 'Adolf'
 assert pavouk_export['druh'] == 'Tarantule Velká'
 assert pavouk_export['vaha'] == 0.1
-
-
 value = getattr(zvire, "vaha")
+"""
 
-
-#-------------------------
+#--------------------------
 #   TŘÍDA ZAMĚSTNANEC
-#-------------------------
+#--------------------------
 
 class Zamestnanec:
     def __init__(self, cele_jmeno: str, rocni_plat: int, pozice: str):
@@ -69,7 +69,6 @@ class Zamestnanec:
 # pokusny_zamestnanec = Zamestnanec("Iveta Eliášková", 50_000, "Manažer")
 # print(pokusny_zamestnanec)
 # print("Iniciály: ", pokusny_zamestnanec.ziskej_inicialy())
-#---------------
 
 zamestnanci_dict = [
     {'cele_jmeno': 'Tereza Vysoka', 'rocni_plat': 700_000, 'pozice': 'Cvičitelka tygrů'},
@@ -81,89 +80,60 @@ seznam_zamestnancu = [Zamestnanec(zamestnanec["cele_jmeno"], zamestnanec["rocni_
 for zamestnanec in seznam_zamestnancu:
     print(zamestnanec)
 
+zamestnanci = [Zamestnanec(**zamestnanec_data) for zamestnanec_data in zamestnanci_dict]
 
 #--------------------------
 #   TŘÍDA ŘEDITEL
 #--------------------------
-"""
-#když dědím z jiné třídy, tak musím zkopírovat: def __init__(všechny atributy, pak sem přidám nový atribut) toto píšu hned
-# pod třídu, pak musím ještě napsat: super().__init__(sem dám všechny atributy z děděné třídy, nedávám sem přidaný atribut)
 
 class Reditel(Zamestnanec):
-                #cele_jmeno: str, rocni_plat: int, pozice: str
-    def __init__(self, cele_jmeno: str, rocni_plat: int, pozice='Reditel', oblibene_zvire= Zvire):
-        super().__init__(jmeno, rocni_plat, pozice='Reditel')
+
+    def __init__(self, cele_jmeno, rocni_plat, pozice, oblibene_zvire: Zvire):
+        super().__init__(cele_jmeno, rocni_plat, pozice)
         self.oblibene_zvire = oblibene_zvire
 
+zvire = Zvire(jmeno="Ruzenka", druh="Panda", vaha=150) # zvire = Zvire(jmeno="Ruzenka", druh="Panda", vaha=150) <- tady je to velke protoze je to objekt tridy Zvire
 
-#zvire = Zvire(jmeno="Tonda", druh="leguán", vaha="4kg")
-zvire = Zvire()
-reditel = Reditel(jmeno='Karel', rocni_plat=800_000, oblibene_zvire=Zvire)
+reditel = Reditel(cele_jmeno='Karel', rocni_plat=800_000, pozice="Reditel", oblibene_zvire=zvire) # reditel = Reditel(cele_jmeno='Karel', rocni_plat=800_000, pozice="Reditel", oblibene_zvire=zvire) <- tady je to male protoze to je jen promenna
 print(reditel)
 print(zvire)
 assert reditel.pozice == 'Reditel'
+assert isinstance(reditel.oblibene_zvire, Zvire)
 
-assert isinstance(reditel.oblibene_zvire, Zvire) 
-"""
-class Reditel(Zamestnanec):
-    def __init__(self, cele_jmeno, rocni_plat, pozice, oblibene_zvire):
-        super().__init__(cele_jmeno, rocni_plat, pozice)
-        self.oblibene_zvire = Zvire
 
-zvire = Zvire()
-reditel = Reditel(jmeno='Karel', rocni_plat=800_000, oblibene_zvire=Zvire)
-print(reditel)
-print(zvire)
-assert reditel.pozice == 'Reditel'
-
-assert isinstance(reditel.oblibene_zvire, Zvire) 
-
-"""
 #--------------------------
-#   TŘÍDA ZOO
+#   TŘÍDA ZOO              
 #--------------------------
+from typing import List
 
-class Zoo(Zamestnanec, Zvire):
-    def __init__(self, jmeno: str, adresa: str, reditel: Reditel, ):
-        super().__init__(Zvire.vaha, Zamestnanec.cele_jmeno, pozice= reditel, zamestnanci= zamestnanci_dict, zvirata= zvirata_dict)
-        # Vpravaé  Zvire.vaha, super().__init__(Zvire.vaha: int, Zamestnanec.cele_jmeno: str) 
+class Zoo():
+    def __init__(self, jmeno: str, adresa: str, reditel: Reditel, zamestnanci: List[Zamestnanec], zvirata: List[Zvire]):
         self.jmeno = jmeno
         self.adresa = adresa
-
-        # self.rocni_plat = rocni_plat
-        # self.pozice = pozice
+        self.reditel = reditel
+        self.zamestnanci = zamestnanci
+        self.zvirata = zvirata
 
     def __str__(self):
-        return f"Název zoo: {self.jmeno}, adresa: {self.adresa}, pozice: {self.pozice}, seznam zaměstnanců {self.zamestnanci_dict }, seznam zvířat {self.zvirata}."   
+        return f"Název zoo: {self.jmeno}, adresa: {self.adresa}, pozice: {self.reditel}, seznam zaměstnanců {self.zamestanci}, seznam zvířat {self.zvirata}."   
 
     def vaha_vsech_zvirat_v_zoo(self):
-        celkova_vaha = sum(zvire.vaha for zvire in self.zvira_dict)
+        celkova_vaha = sum(zvire.vaha for zvire in self.zvirata)
         return celkova_vaha
 
+
     def mesicni_naklady_na_zamestnance(self):
-        mesicni_naklady = sum(zamestnanec.rocni_plat / 12 for zamestnanec in self.zamestnanci)
-        mesicni_naklady += self.reditel.rocni_plat / 12
-        return mesicni_naklady
+          mesicni_naklady = sum(zamestnanec.rocni_plat / 12 for zamestnanec in self.zamestnanci)
+          mesicni_naklady += self.reditel.rocni_plat / 12
+          return mesicni_naklady
 
 
+                                                    
+zoo = Zoo('ZOO Praha', 'U Trojského zámku 3/120', reditel, zamestnanci, zvirata)
 
-# Příklad použití třídy Zoo
-zoo = Zoo(jmeno='ZOO Praha', adresa='U Trojského zámku 3/120')
-# Příklad použití třídy Zoo 
-#zoo = Zoo('ZOO Praha', 'U Trojského zámku 3/120', reditel, zamestnanci_dict, zvirata_dict, zvire.vaha)
-#zoo = Zoo('ZOO Praha', 'U Trojského zámku 3/120', reditel, zamestnanci_dict, zvirata_dict)
-# print(f"jmeno: {zoo.jmeno}")
-#print(zoo)
-
-# for zvire in seznam_zvirat:
-#     print(f"Jméno: {zvire.jmeno}, Druh: {zvire.druh}, Váha: {zvire.vaha} kg")
-
-# pavouk = Zvire(jmeno='Adolf', druh='Tarantule Velká', vaha=0.1)
-
-
-# print(f"Ředitel: {zoo.reditel}")
-#print(f"Celková váha zvířat v ZOO: {zoo.vaha()} kg")
-# print(f"Měsíční náklady na zaměstnance: {zoo.mesicni_naklady_na_zamestnance()} Kč")
+print(zoo.reditel)
+print('Celková váha zvířat v ZOO:', zoo.vaha_vsech_zvirat_v_zoo())
+print('Měsíční náklady na zaměstnance:', zoo.mesicni_naklady_na_zamestnance())
 
 # Zvire class
 zvire = Zvire('Láďa', 'Koala', 15)
@@ -187,22 +157,20 @@ assert zamestnanec.ziskej_inicialy() == 'P.N.'
 
 # Reditel class
 zvire = Zvire('Lev', 'Lvice', 150)
-reditel = Reditel('Jan Novotny', 80000, zvire)
+#reditel = Reditel('Jan Novotny', 80000, zvire)
 assert isinstance(reditel.oblibene_zvire, Zvire)
 
 # Zoo class
-zoologicka = Zoo('Zoo Praha', 'Praha', reditel, [zamestnanec], [zvire])
-assert hasattr(zoo, 'jmeno') #nazev
-# assert hasattr(zoo, 'adresa')
-# assert hasattr(zoo, 'reditel')
-# assert hasattr(zoo, 'zamestnanci')
-# assert hasattr(zoo, 'zvirata')
-# assert isinstance(zoo.nazev, str)
-# assert isinstance(zoo.adresa, str)
-# assert isinstance(zoo.reditel, Reditel)
-# assert isinstance(zoo.zamestnanci, list)
-# assert isinstance(zoo.zvirata, list)
-# assert zoo.vaha_vsech_zvirat_v_zoo() == 150
-# assert zoo.mesicni_naklady_na_zamestnance() == (zamestnanec.rocni_plat + reditel.rocni_plat) / 12
-
-"""
+zoo = Zoo('Zoo Praha', 'Praha', reditel, [zamestnanec], [zvire])
+#assert hasattr(zoo, 'nazev')
+assert hasattr(zoo, 'adresa')
+assert hasattr(zoo, 'reditel')
+assert hasattr(zoo, 'zamestnanci')
+assert hasattr(zoo, 'zvirata')
+#assert isinstance(zoo.nazev, str)
+assert isinstance(zoo.adresa, str)
+assert isinstance(zoo.reditel, Reditel)
+assert isinstance(zoo.zamestnanci, list)
+assert isinstance(zoo.zvirata, list)
+assert zoo.vaha_vsech_zvirat_v_zoo() == 150
+#assert zoo.mesicni_naklady_na_zamestnance() == (zamestnanec.rocni_plat + reditel.rocni_plat) / 12
